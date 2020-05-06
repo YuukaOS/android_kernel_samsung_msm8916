e5002beb2b0dd3e087586220dd296d93fac78a8d
danipragustia <danipragustia@disroot.org>
2020-05-06 16:10:42 +0000

    build: change build script to fortunave3g

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export KBUILD_BUILD_USER=$(id -u -n)
+export KBUILD_BUILD_HOST=$(uname -n)@$(uname -o)
+export CROSS_COMPILE=$(pwd)/../arm-eabi-4.8/bin/arm-eabi-
 
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_gtelwifi_usa_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -j$(nproc) -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_fortunave3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j$(nproc) -C $(pwd) O=output
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+
